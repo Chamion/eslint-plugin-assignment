943d8b474d10bf7a731dbfdcd26f33cc125ceb8b
Jemi Salo <jemi.salo@gmail.com>
2021-06-20 21:39:28 +0300

    initial setup

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    rules: {},
+    configs: {
+        recommended: {
+            plugins: [
+                'assignment'
+            ],
+            rules: {}
+        }
+    }
+};
